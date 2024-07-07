ce5c62852062eccaf4ed401948f6ed1857f9f268
kimkyungsub <cyon256@icloud.com>
2024-07-08 02:13:08 +0900

    basic api request

--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+var baseURI = "https://api.unsplash.com"
+
+type resp struct {
+	Id   string `json:"name"`
+	Akey string `json:"ACCESS_KEY"`
+	Skey string `json:"SECRET_KEY"`
+}
 
 func main() {
-	fmt.Println("hello")
+	file, err := os.Open("key.json")
+	info, err := file.Stat()
+	s := info.Size()
+	if err != nil {
+		panic(err)
+	}
+	data := make([]byte, s)
+	_, err = file.Read(data)
+	env := resp{}
+	err = json.Unmarshal(data, &env)
+	if err != nil {
+		fmt.Printf("json panic %v\n", err)
+	}
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		photo := fmt.Sprintf("%s/photos", baseURI)
+		c := &http.Client{}
+		req, err := http.NewRequest("GET", photo, nil)
+		req.Header.Add("Authorization", "Client-ID "+env.Akey)
+		if err != nil {
+			panic(err)
+		}
+		resp, err := c.Do(req)
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(b)
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
