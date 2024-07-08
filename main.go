156bbf14411aa41bbac7729130ff387829d3f6ef
kimkyungsub <cyon256@icloud.com>
2024-07-08 12:14:08 +0900

    get sqlite3 module

--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/Eirc-lab-star/apiServer/unsplash"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var baseURI = "https://api.unsplash.com"
+
+func main() {
+	keys := parseKeys()
+
+	db, err := sql.Open("sqlite3", "./unsplash.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table Images(id integer not null primary key, fname text, lname text, desc text, urlFull text, urlReg text urlSmall text, urlThumb text);
+	delete from foo;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the Unsplash API"))
+	})
+
+	http.HandleFunc("/photos", func(w http.ResponseWriter, r *http.Request) {
+		photo := fmt.Sprintf("%s/photos", baseURI)
+		b := unsplash.Get(r, keys.Akey, photo)
+		w.Write(b)
+
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
 
 type authKeys struct {
 	Id   string `json:"name"`
@@ -17,7 +54,7 @@ type authKeys struct {
 	Skey string `json:"SECRET_KEY"`
 }
 
-func main() {
+func parseKeys() authKeys {
 	file, err := os.Open("key.json")
 	info, err := file.Stat()
 	s := info.Size()
@@ -26,18 +63,10 @@ func main() {
 	}
 	data := make([]byte, s)
 	_, err = file.Read(data)
-	env := authKeys{}
-	err = json.Unmarshal(data, &env)
+	keys := authKeys{}
+	err = json.Unmarshal(data, &keys)
 	if err != nil {
 		fmt.Printf("json panic %v\n", err)
 	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		photo := fmt.Sprintf("%s/photos", baseURI)
-		b := unsplash.Get(r, env.Akey, photo)
-		w.Write(b)
-	})
-
-	fmt.Println("Server is running on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	return keys
 }
