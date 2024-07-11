e7a8893e05435d0e3d2a8194e33b83e048508890
kimkyungsub <cyon256@icloud.com>
2024-07-11 09:35:03 +0900

    msg go is running

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Eirc-lab-star/apiServer/server"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	fmt.Println("go is runing")
 	server.Start()
 }
